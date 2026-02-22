764db0b84c17d23ce1487555c08020a3c8adaad7
ThierryBab <Thierry.babault.20@hotmail.fr>
2026-02-22 14:41:35 -0500

    Ajout countChars function

--- main.go
+++ main.go
@@ -12,6 +12,16 @@ func countLines(text string) int {
 	return len(strings.Split(text, "\n"))
 }
 
+func countChars(text string) int {
+	count := 0
+	for _, r := range text {
+		if r != ' ' && r != '\n' && r != '\t' {
+			count++
+		}
+	}
+	return count
+}
+
 func main() {
 	text := "Hello\nWorld\nGolang"
 	fmt.Printf("Nombre de lignes : %d\n", countLines(text))
