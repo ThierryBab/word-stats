7f0cc5f8bf4db591b4bfe98513555fd7deda48b2
ThierryBab <Thierry.babault.20@hotmail.fr>
2026-02-22 14:40:53 -0500

    Ajout countWords function

--- main.go
+++ main.go
@@ -12,6 +12,13 @@ func countLines(text string) int {
 	return len(strings.Split(text, "\n"))
 }
 
+func countWords(text string) int {
+	if text == "" {
+		return 0
+	}
+	return len(strings.Fields(text))
+}
+
 func main() {
 	text := "Hello\nWorld\nGolang"
 	fmt.Printf("Nombre de lignes : %d\n", countLines(text))
